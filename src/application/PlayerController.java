0c3c3974893f8f50de2cede2365eea354310ed8a
CassioJr <cassiojuniorceolin@gmail.com>
2021-05-18 11:35:02 -0300

    Implementações novas

--- src/application/PlayerController.java
+++ src/application/PlayerController.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javafx.application.Platform;
 import javafx.collections.MapChangeListener;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert.AlertType;
@@ -18,14 +20,15 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.FileChooser;
 import javafx.util.Duration;
-
 public class PlayerController {
 	@FXML
 	private Label musicname;
 	@FXML
 	private Label duration;
 	@FXML
-	private ProgressBar barraProgreso;
+	private Label current;
+	@FXML
+	private Slider barraProgresso;
 	@FXML
 	private ImageView albumImage;
 	@FXML
@@ -34,8 +37,8 @@ public class PlayerController {
 	private boolean running = false;
 	private MediaPlayer mp;
 	private Media midia;
-	//private Timer timer;
-	//private TimerTask task;
+	private Timer timer;
+	private TimerTask task;
 
 	public void AlbumCover() {
 		File f = new File("imagens//nocoverart.jpg");
@@ -54,6 +57,25 @@ public class PlayerController {
 		});
 	}
 
+	public void durationTotal() {
+		mp.setOnReady(new Runnable() {
+			@Override
+			public void run() {
+				int durationTime = (int) midia.getDuration().toMillis();
+				int minutes = (durationTime / 60000) % 60000;
+				int seconds = durationTime % 60000 / 1000;
+				String MusicTime = String.format("%02d:%02d", minutes, seconds);
+				duration.setText(MusicTime);
+
+				int Time = (int) mp.getCurrentTime().toMillis();
+				int minutescurrent = (Time / 60000) % 60000;
+				int secondscurrent = Time % 60000 / 1000;
+				String CurrentTime = String.format("%02d:%02d", minutescurrent, secondscurrent);
+				current.setText(CurrentTime);
+				}
+		});
+	}
+
 	public void alertMessage(String m) {
 		Alert alert = new Alert(AlertType.WARNING);
 		alert.setTitle("Atenção");
@@ -63,17 +85,40 @@ public class PlayerController {
 	}
 
 	public void Progresso() {
+	/*	timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+				running = true;
+				double current = mp.getCurrentTime().toSeconds();
+				double end = midia.getDuration().toSeconds();
+				barraProgresso.setMax(end);
+				barraProgresso.setMin(0);
+				barraProgresso.setValue(current/end);
+			});
 
-		/*
-		 * timer = new Timer(); task = new TimerTask() {
-		 * 
-		 * public void run() {
-		 * 
-		 * running = true; double current = mp.getCurrentTime().toSeconds(); double end
-		 * = midia.getDuration().toSeconds(); barraProgreso.setProgress(current/end);
-		 * 
-		 * } }; timer.scheduleAtFixedRate(task, 0, 1000);
-		 */
+			}
+		}, 0, 1000);
+*/
+	}
+	
+	
+	public void currentTime() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+					int Time = (int) mp.getCurrentTime().toMillis();
+					int minutes = (Time / 60000) % 60000;
+					int seconds = Time % 60000 / 1000;
+					String CurrentTime = String.format("%02d:%02d", minutes, seconds);
+					current.setText(CurrentTime);
+				});
+
+			}
+		}, 0, 1000);
 	}
 
 	public void play() {
@@ -81,10 +126,10 @@ public class PlayerController {
 			if (running == false) {
 				midia = new Media(Paths.get(caminhomusica).toUri().toString());
 				mp = new MediaPlayer(midia);
-				Progresso();
 				mp.play();
 				button.setText("Pause");
 				running = true;
+			    currentTime();
 			} else {
 				mp.pause();
 				button.setText("Play");
@@ -95,6 +140,7 @@ public class PlayerController {
 		}
 	}
 
+
 	public void MusicFile() {
 		try {
 			FileChooser fc = new FileChooser();
@@ -102,24 +148,29 @@ public class PlayerController {
 			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("MP3", "*.mp3*"));
 			File file = fc.showOpenDialog(null);
 			if (file != null) {
-			musicname.setText(file.getName().replace(".mp3", ""));
-			caminhomusica = file.getAbsolutePath();
-			play();
-			AlbumCover();
+				musicname.setText(file.getName().replace(".mp3", ""));
+				caminhomusica = file.getAbsolutePath();
+				play();
+				AlbumCover();
+				durationTotal();
+				Progresso();
+			}
+			
+		}catch(NullPointerException e) {
+			
+		} catch (Exception e) {
+			System.out.println("Arquivo inexistente");
 		}
-	} catch (Exception e) {
-		System.out.println("Arquivo inexistente");
-	}
 	}
 
 	public void Caminho() {
-		
-			if (running == true) {
-				play();
-				MusicFile();
-			} else {
-				MusicFile();
-		
+
+		if (running == true) {
+			play();
+			MusicFile();
+		} else {
+			MusicFile();
+
+		}
 	}
-			}
 }
